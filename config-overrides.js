0a572671fad1dcdfb9b3a522121814908e53d9fc
matej <matej.basta@email.cz>
2024-04-27 12:25:01 +0200

    updated production url

--- config-overrides.js
+++ config-overrides.js
@@ -5,7 +5,7 @@ const deps = require('./package.json').dependencies;
 
 module.exports = function override(config, env) {
     // Set the public path based on the environment
-    const publicPath = env === 'development' ? 'http://localhost:3009/' : 'something';
+    const publicPath = env === 'development' ? 'http://localhost:3009/' : 'https://mf-cart.vercel.app/';
 
     // Ensure the public path is set in the output configuration
     config.output = {
